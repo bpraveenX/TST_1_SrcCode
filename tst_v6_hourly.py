6cf1b2c27ecff89f2e11727262384facb61834b1
bpraveenX <147453228+bpraveenX@users.noreply.github.com>
2024-01-08 09:41:03 -0500

    Update tst_v6_hourly.py

--- tst_v6_hourly.py
+++ tst_v6_hourly.py
@@ -101,7 +101,7 @@ discordChLink = cred_file.iloc[1][0].split('=')[1].strip()
 authCode = cred_file.iloc[2][0].split('=')[1].strip()
 portNum = cred_file.iloc[3][0].split('=')[1].strip()
 qty = cred_file.iloc[4][0].split('=')[1].strip()
-contractName = qty = cred_file.iloc[5][0].split('=')[1].strip()
+contractName = cred_file.iloc[5][0].split('=')[1].strip()
 
 # TTB channel
 webhook = SyncWebhook.from_url(webhook_link)
