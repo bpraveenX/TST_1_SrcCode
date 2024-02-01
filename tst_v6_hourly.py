7619d51850cc61ed7eef320285172dc90cf312a9
bpraveenX <147453228+bpraveenX@users.noreply.github.com>
2024-02-01 10:10:51 -0500

    Update tst_v6_hourly.py

--- tst_v6_hourly.py
+++ tst_v6_hourly.py
@@ -429,7 +429,7 @@ def main():
         except:
             time.sleep(.5)
             
-    df[1] = pd.to_datetime(df[1])
+    df[1] = pd.to_datetime(df[1],format = 'mixed')
     # df[1] = df[1].apply(lambda x:str(x)[:19])
     customUTC = timeInUTC.replace(minute = 0,second = 0)
     dffilt = df[df[1]>=customUTC]
