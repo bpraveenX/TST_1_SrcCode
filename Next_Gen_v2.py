37617ef9fe6db748ce21fa1741d577f90a7f54a9
nlovelace1985 <129923913+nlovelace1985@users.noreply.github.com>
2024-04-08 10:51:01 -0400

    Next_Gen_v2.py

--- Next_Gen_v2.py
+++ Next_Gen_v2.py
@@ -30,10 +30,433 @@ import pytz
 import sys
 
 # ... (the rest of the code remains the same)
+from discord import SyncWebhook
+
+def send_discord_message(message):
+    webhook.send(message)
+    
+expiryValue = '202406'
+cred_file = pd.read_csv('C:\\Users\\Administrator\\Downloads\\discord_cred_text.txt', header = None)
+webhook_link = cred_file.iloc[0][0].split('=')[1].strip()
+discordChLink = cred_file.iloc[1][0].split('=')[1].strip()
+authCode = cred_file.iloc[2][0].split('=')[1].strip()
+portNum = cred_file.iloc[3][0].split('=')[1].strip()
+qty = cred_file.iloc[4][0].split('=')[1].strip()
+contractName = cred_file.iloc[5][0].split('=')[1].strip()
+
+webhook = SyncWebhook.from_url(webhook_link)
+discordChannel = discordChLink
+authorizationCode = authCode
+
+
+def retrieve_messages():
+    headers = {
+        'authorization':authorizationCode
+        }
+    
+    r = requests.get(discordChannel,headers = headers)
+    
+    jobj = json.loads(r.text)
+    i = 0
+    df = pd.DataFrame()
+    for value in jobj:
+        # print(value) 
+        i += 1
+        if i > 2:
+            break
+        # print(value['content'],"\n")
+        df = pd.concat([df,pd.DataFrame([value['content'],value['timestamp']]).transpose()])
+        
+    return df
+        
+        
 
 def main():
     # ... (the rest of the code remains the same)
+    class TradeApp(EWrapper, EClient):  # creating the tradeapp class with historicalData to be printed in dataframe
+        def __init__(self): 
+            EClient.__init__(self, self) 
+            self.acc_summary = pd.DataFrame(columns=['ReqId', 'Account', 'Tag', 'Value', 'Currency'])
+            self.pnl_summary = pd.DataFrame(columns=['ReqId', 'DailyPnL', 'UnrealizedPnL', 'RealizedPnL'])
+            self.order_df = pd.DataFrame(columns=['PermId', 'ClientId', 'OrderId',
+                                                  'Account', 'Symbol', 'SecType',
+                                                  'Exchange', 'Action', 'OrderType',
+                                                  'TotalQty', 'CashQty', 'LmtPrice',
+                                                  'AuxPrice', 'Status'])
+            self.pos_df = pd.DataFrame(columns=['Account', 'Symbol', 'SecType',
+                                                'Currency', 'Position', 'Avg cost'])
+    
+        def accountSummary(self, reqId, account, tag, value, currency):
+            super().accountSummary(reqId, account, tag, value, currency)
+            dictionary = {"ReqId":reqId, "Account": account, "Tag": tag, "Value": value, "Currency": currency}
+            # self.acc_summary = self.acc_summary.append(dictionary, ignore_index=True)
+            d1 = pd.DataFrame([dictionary])
+            d2 = pd.concat([self.acc_summary,d1])
+            self.acc_summary = d2
+            
+        def pnl(self, reqId, dailyPnL, unrealizedPnL, realizedPnL):
+            super().pnl(reqId, dailyPnL, unrealizedPnL, realizedPnL)
+            dictionary = {"ReqId":reqId, "DailyPnL": dailyPnL, "UnrealizedPnL": unrealizedPnL, "RealizedPnL": realizedPnL}
+            # self.pnl_summary = self.pnl_summary.append(dictionary, ignore_index=True)
+            d1 = pd.DataFrame([dictionary])
+            d2 = pd.concat([self.pnl_summary,d1])
+            self.pnl_summary = d2
+            
+        def nextValidId(self, orderId):
+            super().nextValidId(orderId)
+            self.nextValidOrderId = orderId
+            print("NextValidId:", orderId)
+            
+        def openOrder(self, orderId, contract, order, orderState):
+            super().openOrder(orderId, contract, order, orderState)
+            dictionary = {"PermId":order.permId, "ClientId": order.clientId, "OrderId": orderId, 
+                          "Account": order.account, "Symbol": contract.symbol, "SecType": contract.secType,
+                          "Exchange": contract.exchange, "Action": order.action, "OrderType": order.orderType,
+                          "TotalQty": order.totalQuantity, "CashQty": order.cashQty, 
+                          "LmtPrice": order.lmtPrice, "AuxPrice": order.auxPrice, "Status": orderState.status}
+            # self.order_df = self.order_df.append(dictionary, ignore_index=True)
+            d1 = pd.DataFrame([dictionary])
+            d2 = pd.concat([self.order_df,d1])
+            self.order_df = d2
+            
+        ######### uncomment code below if you want to see the data downloaded (historic)
+        def historicalData(self, reqId, bar):
+            if reqId not in self.data:
+                self.data[reqId] = pd.DataFrame([{"Date":bar.date,"Open":bar.open,"High":bar.high,"Low":bar.low,"Close":bar.close,"Volume":bar.volume}])
+            else:
+                self.data[reqId] = pd.concat((self.data[reqId],pd.DataFrame([{"Date":bar.date,"Open":bar.open,"High":bar.high,"Low":bar.low,"Close":bar.close,"Volume":bar.volume}])))
+                #self.data[reqId].append({"Date":bar.date,"Open":bar.open,"High":bar.high,"Low":bar.low,"Close":bar.close,"Volume":bar.volume})
+            # print("reqID:{}, date:{}, open:{}, high:{}, low:{}, close:{}, volume:{}".format(reqId,bar.date,bar.open,bar.high,bar.low,bar.close,bar.volume))
+            
+        def position(self, account, contract, position, avgCost):
+            super().position(account, contract, position, avgCost)
+            dictionary = {"Account":account, "Symbol": contract.symbol, "SecType": contract.secType,
+                          "Currency": contract.currency, "Position": position, "Avg cost": avgCost}
+            # self.pos_df = self.pos_df.append(dictionary, ignore_index=True)
+            d1 = pd.DataFrame([dictionary])
+            d2 = pd.concat([self.pos_df,d1])
+            self.pos_df = d2
+    
+    def usTechStk(symbol,expiry,sec_type="FUT",currency="USD",exchange="CME"):
+        # ES is pulled from the CME exchange, not GLOBEX
+        contract = Contract()
+        contract.symbol = symbol
+        contract.secType = sec_type
+        contract.currency = currency
+        contract.exchange = exchange
+        contract.lastTradeDateOrContractMonth = expiry
+        return contract 
+    
+    def histData(req_num,contract,duration,candle_size):
+        """extracts historical data"""
+        app.reqHistoricalData(reqId=req_num, 
+                              contract=contract,
+                              endDateTime='',
+                              durationStr=duration,
+                              barSizeSetting=candle_size,
+                              whatToShow='ADJUSTED_LAST',
+                              useRTH=0,
+                              formatDate=1,
+                              keepUpToDate=0,
+                              chartOptions=[])	 # EClient function to request contract details
+        
+    #creating object of the limit order class - will be used as a parameter for other function calls
+    def limitOrder(direction,quantity,lmt_price):
+        order = Order()
+        order.action = direction
+        order.orderType = "LMT"
+        order.totalQuantity = quantity
+        order.lmtPrice = lmt_price
+        order.tif = "GTC" 
+        order.eTradeOnly = False 
+        order.firmQuoteOnly = False 
+        return order
+    
+    def websocket_con():
+        app.run()
+        
+    #creating object of the Contract class - will be used as a parameter for other function calls
+    def placeTradeContract(symbol,sec_type="FUT",currency="USD",exchange="CME"):
+        contract = Contract()
+        contract.symbol = symbol
+        contract.secType = sec_type
+        contract.currency = currency
+        contract.exchange = exchange
+        return contract 
+    
+    def marketOrder(direction,quantity):
+        order = Order()
+        order.action = direction
+        order.orderType = "MKT"
+        order.totalQuantity = quantity
+        order.eTradeOnly = ''
+        order.firmQuoteOnly = ''
+        order.tif = 'GTC'
+        order.outsideRth = True
+        return order
+    
+    ###################storing trade app object in dataframe#######################
+    def dataDataframe(symbols,TradeApp_obj):
+        "returns extracted historical data in dataframe format"
+        df_data = {}
+        for symbol in symbols:
+            df_data[symbol] = pd.DataFrame(TradeApp_obj.data[symbols.index(symbol)])        
+            df_data[symbol].set_index("Date",inplace=True)
+            #if you need to change the timezone of the candles, uncomment below line and change the time zones accordingly
+            #df_data[symbol].index = pd.to_datetime(df_data[symbol].index, format='%Y%m%d %H:%M:%S %Z')
+            #df_data[symbol].index= pd.DatetimeIndex(df_data[symbol].index).tz_convert("America/Indiana/Petersburg")
+        return df_data
+    
+    #creating object of the bracket order class - will be used as a parameter for other function calls
+    #creating object of the bracket order class - will be used as a parameter for other function calls
+    def bktOrder(order_id,direction,quantity,lmt_price, sl_price, tp_price):
+        """
+        Parameters
+        ----------
+        order_id : Order ID value
+        direction : Buy or Sell
+        quantity : qty
+        lmt_price : limit price
+        sl_price : stop loss price
+        tp_price : take profit price
+    
+        Returns
+        -------
+        bracket_order : bracket order
+    
+        """
+        
+        ordersummary = pd.DataFrame()
+        
+        parent = Order()
+        parent.orderId = order_id
+        parent.action = direction
+        parent.orderType = "LMT"
+        parent.totalQuantity = quantity
+        parent.lmtPrice = lmt_price
+        parent.transmit = False
+        parent.eTradeOnly = ''
+        parent.firmQuoteOnly = ''
+        parent.tif = "GTC"
+        parent.outsideRth = True
+        tdf = pd.DataFrame([order_id,direction])
+        ordersummary = pd.concat([ordersummary,tdf.transpose()])
+        
+        
+        slOrder = Order()
+        slOrder.orderId = parent.orderId + 1
+        slOrder.action =  "SELL" if direction == "BUY" else "BUY"
+        slOrder.orderType = "STP" 
+        slOrder.totalQuantity = quantity
+        slOrder.auxPrice = sl_price
+        slOrder.parentId = order_id
+        slOrder.transmit = False
+        slOrder.eTradeOnly = ''
+        slOrder.firmQuoteOnly = ''
+        slOrder.tif = "GTC"
+        slOrder.outsideRth = True
+        tdf = pd.DataFrame([parent.orderId + 1,slOrder.action])
+        ordersummary = pd.concat([ordersummary,tdf.transpose()])
+        
+        tpOrder = Order()
+        tpOrder.orderId = parent.orderId + 2
+        tpOrder.action = "SELL" if direction == "BUY" else "BUY"
+        tpOrder.orderType = "LMT"
+        tpOrder.totalQuantity = quantity
+        tpOrder.lmtPrice = tp_price
+        tpOrder.parentId = order_id
+        tpOrder.transmit = True
+        tpOrder.eTradeOnly = ''
+        tpOrder.firmQuoteOnly = ''
+        tpOrder.tif = "GTC"
+        tpOrder.outsideRth = True
+        tdf = pd.DataFrame([parent.orderId + 2,tpOrder.action])
+        ordersummary = pd.concat([ordersummary,tdf.transpose()])
+        ordersummary.to_excel('ordersummary.xlsx')
+        bracket_order = [parent, slOrder, tpOrder]
+        # sample code : bracket = bktOrder(app.nextValidOrderId,"BUY",10,85,75,95)
+        return bracket_order
+    
+    # app.placeOrder(order_id,usTechStk("FB"),marketOrder("BUY",1))
+    
+    clientId = 1
+    
+    app = TradeApp()
+    app.connect(host='127.0.0.1', port=int(portNum), clientId=clientId) #port 4002 for ib gateway paper trading/7497 for TWS paper trading
+    con_thread = threading.Thread(target=websocket_con, daemon=True)
+    con_thread.start()
+    time.sleep(1) # some latency added to ensure that the connection is established
+    
+    def round_nearest_qtr(number):
+        base = 0.25
+        return round(base * round(number/base),2)
+    
+    tickers = ["ES"]
+    contract = Contract()
+    contract.symbol = contractName
+    contract.secType = "FUT"
+    contract.exchange = "CME"
+    contract.currency = "USD"
+    contract.lastTradeDateOrContractMonth = expiryValue  # Replace with your desired expiration date
+    
+    retryConnection = 0
+    breakcode = 0 # to exit code if any issues are found and send discord message
+    
+    while True:
+        if breakcode ==1 :
+            send_discord_message('Please resolve issues and re run code, or it will next start automatically in an hour.')
+            time.sleep(.5)
+            break
+        try:
+            ordernum  = app.nextValidOrderId
+            print('established connection..')
+            msg1 = "Connection established. Exit time is: "+str(exitTime)
+            send_discord_message(msg1)
+            break
+        except Exception as e:
+            print(e)
+            retryConnection += 1
+            clientId += 1
+            del app
+            del con_thread
+            time.sleep(1)
+            print('restarting TWS connection')
+            app = TradeApp()
+            app.connect(host='127.0.0.1', port=int(portNum), clientId=clientId) #port 4002 for ib gateway paper trading/7497 for TWS paper trading
+            con_thread = threading.Thread(target=websocket_con, daemon=True)
+            con_thread.start()
+            time.sleep(1) # some latency added to ensure that the connection is established
+            app.reqIds(-1)
+            try:
+                ordernum  = app.nextValidOrderId
+                print(ordernum)
+                print('established connection..')
+                msg1 = "Connection established. Exit time is: "+str(exitTime)
+                send_discord_message(msg1)
+                time.sleep(.2)
+                break
+            except:
+                print('retrial: ',retryConnection)
+                if retryConnection > 10:
+                    breakcode = 1
+                    send_discord_message("Error with TWS Connection. Please check if account is logged in, or connect with our representatives.")
+                    time.sleep(.5)
+                    break
+                pass
+            # if retryConnection > 5:
+            #     print('couldnt establish connection, please check manually')
+            #     cstr = 'couldnt establish connection, please check manually'
+            #     send_discord_message(cstr)
+            #     time.sleep(.4)
+            #     break
+            
+            
+    prevmsg = ''
+    error_inc = 0
+    folder_time = datetime.now().strftime("%Y-%m-%d")
+    
+    
+    # timezone identification
+    newYorkTz = pytz.timezone("US/Eastern") #New_York
+    UtcTz = pytz.timezone("UTC") #New_York
+    timeInNewYork = datetime.now(newYorkTz)
+    timeInUTC = datetime.now(UtcTz)
+    utcHour = timeInUTC.hour
+    currentTimeInNewYork = timeInNewYork.strftime("%H:%M:%S")
+    currentTimeInUTC = timeInUTC.strftime("%H:%M:%S")
+    systemTime = datetime.now()
+    systemTime - pd.to_datetime(currentTimeInUTC)
+    pd.to_datetime(currentTimeInUTC) - systemTime
+    
+    deltaHours = 0
+    
+    # order_id = app.nextValidOrderId
+    # app.placeOrder(order_id, contract, marketOrder("BUY",1))
+    
+    if (systemTime - pd.to_datetime(currentTimeInUTC)) < timedelta(minutes = 1):
+        deltaHours = 0 
+    elif (systemTime - pd.to_datetime(currentTimeInNewYork)) < timedelta(minutes = 1):
+        deltaHours = 4 
+    elif (systemTime - pd.to_datetime(currentTimeInNewYork)) > timedelta(hours = 2, minutes = 59):
+        deltaHours = 7
+    elif (systemTime - pd.to_datetime(currentTimeInNewYork)) > timedelta(hours = 1, minutes = 59):
+        deltaHours = 6
+    elif (systemTime - pd.to_datetime(currentTimeInNewYork)) > timedelta(minutes = 59):
+        deltaHours = 5 
+    
+        
+    crntmsg = '1'
+    prevmsg = '2'
+    
+    while crntmsg != prevmsg:
+        try:
+            msg = retrieve_messages()
+            crntmsg = msg.iloc[0][0]
+            prevmsg = crntmsg
+            print(crntmsg)
+            crnthour = timeInNewYork.hour
+            prevhour = crnthour
+            crntmin = timeInNewYork.minute
+            prevmin = crntmin
+            dttemp = datetime.now()
+            # if dttemp.minute < 29:
+            #     exitTime = dttemp.replace(minute = 29, second = 58)
+            # else:
+            #     exitTime = dttemp.replace(minute = 59, second = 58)#pd.to_datetime(currentTimeInNewYork) + timedelta(minutes = 59, seconds = 50)
+            print('exitTime is ',exitTime)
+        except:
+            time.sleep(.5)
+        
+    # print('exitTime is ',exitTime)
+    # code to handle trades that are placed exactly during the code refresh time
+    # check for edge of hourly messages missing trades
+    headers = {
+        'authorization':authorizationCode
+        }
 
+    df = pd.DataFrame()
+    while len(df) == 0:
+        try:
+            r = requests.get(discordChannel,headers = headers)
+            
+            jobj = json.loads(r.text)
+            i = 0
+            
+            # code for the edge case of hourly messages
+            
+            # first reading when previous code shutdown
+            
+            for value in jobj:
+                i += 1
+                print(value['content'],"\n")
+                df = pd.concat([df,pd.DataFrame([value['content'],value['timestamp']]).transpose()]) # contains discord messages
+                if i > 4:
+                    break
+        except:
+            time.sleep(.5)
+            
+    df[1] = pd.to_datetime(df[1])
+    # df[1] = df[1].apply(lambda x:str(x)[:19])
+    customUTC = timeInUTC.replace(minute = 0,second = 0)
+    dffilt = df[df[1]>=customUTC]
+    
+    print('dffilt is :')
+    print(dffilt)
+    if len(dffilt)>0:
+        
+        for i in range(0,len(dffilt)):
+            # print(dffilt.iloc[i][0])
+            if 'ES1' in dffilt.iloc[i][0]:
+                print('in loop:',i)
+                crntmsg = dffilt.iloc[i][0]
+                print('ES1! trade found during transition!!')
+                # place order
+                print(crntmsg)
+                
+                
+    ####################
+    
+    
     while datetime.now() < exitTime:
         if breakcode == 1:
             break
@@ -151,10 +574,12 @@ def main():
                 # error_payload = {'content': f"Error in script: {error_message}"}
                 #df3 = pd.DataFrame([e])
                 #df3.to_csv(errorFile, header=None, index=None, sep=' ', mode='a')
+                # print('in error loop')
                 time.sleep(1) # to give it a min and see if we can re run code.
             else:
                 break
         
+        time.sleep(1)
         
     # time.sleep(1)
     print([crnthour,prevhour])
@@ -170,3 +595,4 @@ if __name__ == '__main__':
     timeInUTC = datetime.now(UtcTz)
     currentTimeInNewYork = timeInNewYork.strftime("%H:%M:%S")
     currentTimeInUTC = timeInUTC.strftime("%H:%M:%S")
+
