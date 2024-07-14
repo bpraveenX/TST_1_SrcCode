3f0eebd047e6ac0944817083ab478ebcd652f480
bpraveenX <147453228+bpraveenX@users.noreply.github.com>
2024-07-14 10:02:21 -0400

    Add files via upload

---
+++ tst_v9_nutika.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'tst_v9_nutika.exe' with Python DLL being found.
+set PATH=C:\Users\prave\ANACON~1;%PATH%
+set PYTHONHOME=C:\Users\prave\anaconda3
+set NUITKA_PYTHONPATH=C:\Users\prave\OneDrive\Documents\Freelancer\oldNicCodes;C:\Users\prave\anaconda3\DLLs;C:\Users\prave\anaconda3\Lib;C:\Users\prave\anaconda3;C:\Users\prave\AppData\Roaming\Python\Python311\site-packages;C:\Users\prave\anaconda3\Lib\site-packages;C:\Users\prave\anaconda3\Lib\site-packages\win32;C:\Users\prave\anaconda3\Lib\site-packages\win32\lib;C:\Users\prave\anaconda3\Lib\site-packages\Pythonwin
+"%~dp0tst_v9_nutika.exe" %*
